66183ca719e3989da604df70bf5ce2d00673df3f
Larry Jr <larry.g3284@yahoo.com>
2018-01-28 05:46:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,6 +24,10 @@ function appendKitten(name) {
   return [...kittens, name];
 }
 
+function prependKitten(name) {
+  return [name, ...kittens];
+}
+
 function removeLastKitten(name) {
   return kittens.slice(0, kittens.length - 1)
 }
