d670bf1dcc44c05dc6706e151028651f361e6b59
Larry Jr <larry.g3284@yahoo.com>
2018-01-28 05:48:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const app = "I don't do much."
 
 function destructivelyAppendKitten(name) {
   kittens.push(name);
-  return kitens;
+  return kittens;
 }
 
 function destructivelyPrependKitten(name) {
